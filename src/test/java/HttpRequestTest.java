b1cbc35f7f9f51abc83512d4cffd9415b65d65f8
minhaz(진민화) <minhaz@coupang.com>
2018-03-04 17:36:14 +0900

    HttpRequest Class + Test Code 구현

---
+++ src/test/java/HttpRequestTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import webserver.HttpRequest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import static org.junit.Assert.*;
+
+public class HttpRequestTest {
+	private String testDir = "./src/test/resources/";
+
+	@Test
+	public void request_GET() throws Exception {
+		InputStream in = new FileInputStream(new File(testDir + "Http_GET.txt"));
+		HttpRequest request = new HttpRequest(in);
+
+		assertEquals("GET", request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("mhjin", request.getParameter("userId"));
+	}
+
+	@Test
+	public void request_POST() throws Exception {
+		InputStream in = new FileInputStream(new File(testDir + "Http_POST.txt"));
+		HttpRequest request = new HttpRequest(in);
+
+		assertEquals("POST", request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("mhjin", request.getParameter("userId"));
+	}
+}
---
+++ src/test/java/HttpResponseTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import static org.junit.Assert.*;
+
+public class HttpResponseTest {
+
+	private String testDir = "./src/test/resources/";
+//
+//	@Test
+//	public void responseForward() throws Exception {
+//		// Http_Forward.txt 결과는 응답 body에 index.html이 포함되어 있어야 한다
+//		HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
+//		response.forward("/index.html");
+//	}
+//
+//	@Test
+//	public void responseRedirect() throws Exception {
+//		HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
+//		response.sendRedirect("/index.html");
+//	}
+//
+//	@Test
+//	public void responseCookies() throws Exception {
+//		HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
+//		response.addHeader("Set-Cookie", "logined=true");
+//		response.sendRedirect("/index.html");
+//	}
+//
+//	private Object createOutputStream(String fileName) throws FileNotFoundException {
+//		return new FileOutputStream(new File(testDir + fileName));
+//	}
+
+}
